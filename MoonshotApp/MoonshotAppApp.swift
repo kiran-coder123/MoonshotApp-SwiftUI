e96a75ec882beb8c1934a06246f419f439c8d48b
kiran sonne <kiransonne04@gmail.com>
2022-11-23 17:44:53 +0530

    Initial Commit

---
+++ MoonshotApp/MoonshotAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotAppApp.swift
+//  MoonshotApp
+//
+//  Created by Kiran Sonne on 23/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
